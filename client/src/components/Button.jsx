024a2b31ac8522b68a73fb058745705a8d7f6049
jyotiraditya <jyotiraditya24@gmail.com>
2023-05-27 18:53:13 +0530

    Register Page made using react-hook-form

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,11 @@
+const Button = ({ label = "click" }) => {
+  return (
+    <button
+      className="px-4 py-2 bg-gradient-to-r from-yellow-200 to-cyan-500 rounded-xl 
+      hover:bg-gradient-to-r hover:scale-105 transition duration-150 focus:text-black hover:font-bold"
+    >
+      {label}
+    </button>
+  );
+};
+export default Button;
