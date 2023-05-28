debf85d85ff9302a689b9844ee08e95b12d038af
jyotiraditya <jyotiraditya24@gmail.com>
2023-05-29 04:47:11 +0530

    Added logic to protect the routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,26 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import LoginPage from "./pages/loginPage";
 import ProfilePage from "./pages/profilepage";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 function App() {
-  const mode = useSelector((state)=>{return state.mode});
+  const mode = useSelector((state) => {
+    return state.mode;
+  });
+  const isAuth = useSelector((state) => state.token);
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
